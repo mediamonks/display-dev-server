f96c8432d600434361fcaa0813515439c558c2c9
Mirko van Willigenburg <mirko@mediamonks.com>
2023-01-10 10:05:52 +0100

    adds new react preview

--- src/webpack/buildFiles.js
+++ src/webpack/buildFiles.js
@@ -1,14 +1,20 @@
-const chalk = require('chalk');
-const webpack = require('webpack');
+const chalk = require("chalk");
+const webpack = require("webpack");
 
-const fs = require('fs-extra');
-const path = require('path');
-const globPromise = require('glob-promise');
-const cliProgress = require('cli-progress');
+const fs = require("fs-extra");
+const path = require("path");
+const globPromise = require("glob-promise");
+const cliProgress = require("cli-progress");
+const archiver = require("archiver");
 
+const getTemplate = require("../util/getPreviewTemplate");
+const removeTempRichmediaRc = require("../util/removeTempRichmediaRc");
 
-const getTemplate = require('../util/getPreviewTemplate');
-const removeTempRichmediaRc = require('../util/removeTempRichmediaRc');
+const getFilesizeInBytes = (filename) => {
+  var stats = fs.statSync(filename);
+  var fileSizeInBytes = stats.size;
+  return fileSizeInBytes;
+};
 
 module.exports = async function buildFiles(result, buildTarget, chunkSize = 10) {
   const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
@@ -16,7 +22,7 @@ module.exports = async function buildFiles(result, buildTarget, chunkSize = 10) 
   let buildResult = [];
 
   function webpackRun(config) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       webpack(config.webpack).run((err, stats) => {
         if (err) {
           console.error(err.stack || err);
@@ -37,7 +43,7 @@ module.exports = async function buildFiles(result, buildTarget, chunkSize = 10) 
         }
 
         if (stats.hasWarnings()) {
-          info.warnings.forEach(item => {
+          info.warnings.forEach((item) => {
             console.log(chalk.green(item.message));
           });
         }
@@ -47,17 +53,16 @@ module.exports = async function buildFiles(result, buildTarget, chunkSize = 10) 
   }
 
   const resultChunks = result.reduce((resultArray, item, index) => {
-    const chunkIndex = Math.floor(index/chunkSize)
-    if(!resultArray[chunkIndex]) {
-      resultArray[chunkIndex] = [] // start a new chunk
+    const chunkIndex = Math.floor(index / chunkSize);
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
     }
-    resultArray[chunkIndex].push(item)
-    return resultArray
+    resultArray[chunkIndex].push(item);
+    return resultArray;
   }, []);
 
-
   for (const [index, resultChunk] of resultChunks.entries()) {
-    const promiseArray = resultChunk.map( result => {
+    const promiseArray = resultChunk.map((result) => {
       return webpackRun(result);
     });
 
@@ -69,43 +74,46 @@ module.exports = async function buildFiles(result, buildTarget, chunkSize = 10) 
 
   progressBar.stop();
 
-  const template = await getTemplate();
-  const templateConfig = {
-    banner: result.map((result) => {
+  // copy preview folder
+  fs.copySync(path.join(__dirname, `../preview/`), buildTarget, {
+    overwrite: true,
+  });
 
-      let bundleName = /[^/\\]*$/.exec(result.webpack.output.path)[0]
-      let width = result.settings.data.settings.size.width;
-      let height = result.settings.data.settings.size.height;
-      const isDevelopment = false;
-
+  // create the ads.json
+  const adsList = {
+    ads: result.map((result) => {
       return {
-        src: `./${bundleName}/`,
-        name: bundleName,
-        title: bundleName,
-        width,
-        height,
-        isDevelopment,
+        width: result.settings.data.settings.size.width,
+        height: result.settings.data.settings.size.height,
+        bundleName: result.settings.data.settings.bundleName,
+        output: {
+          html: {
+            url: `${result.settings.data.settings.bundleName}/index.html`,
+            optimizations: result.settings.data.settings.optimizations,
+          },
+          zip: {
+            url: `${result.settings.data.settings.bundleName}.zip`,
+            size: getFilesizeInBytes(`./${buildTarget}/${result.settings.data.settings.bundleName}.zip`),
+          },
+        },
       };
     }),
   };
 
-  // move static files to folder
-  await fs.mkdir(`${buildTarget}/static/`);
-  const staticPreviewFiles = ['gsap.min.js', 'GSDevTools.min.js', 'main.js', 'material-design.css', 'material-design.js', 'style.css'];
-  await staticPreviewFiles.forEach(filename => {
-    fs.copyFile(path.join(__dirname, `../data/static/`)+filename, `${buildTarget}/static/${filename}`, (err) => {
-      if (err) throw err;
-    });
-  });
+  // get a list of all the zip files
+  //const zipFiles = adsList.map((result) => {
+  //  return output.zip.url;
+  //});
 
-  //return built index.html
-  await fs.outputFile(`${buildTarget}/index.html`, template(templateConfig));
+  // write the result to ads.json in the preview dir
+  fs.writeFileSync(`${buildTarget}/data/ads.json`, JSON.stringify(adsList, null, 2));
 
-  console.log('Removing temp .richmediarc...')
+  // final clean up
+  console.log("Removing temp .richmediarc...");
   removeTempRichmediaRc(result);
 
   return {
     buildTarget: path.resolve(buildTarget),
-    ads: buildResult
-  }
-}
+    ads: buildResult,
+  };
+};
