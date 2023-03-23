67425d33a49934affe4f997eaf92ed08da328d93
Mirko van Willigenburg <mirko@mediamonks.com>
2023-03-23 10:02:51 +0100

    slightly refactored richmediaRcLoader

--- src/webpack/loader/RichmediaRCLoader.js
+++ src/webpack/loader/RichmediaRCLoader.js
@@ -1,17 +1,17 @@
 // const loaderUtils = require('loader-utils');
 const isExternalURL = require('../../util/isExternalURL');
-const getRichmediaRC = require('../../util/getRichmediaRC');
 const leafs = require('../../util/leafs');
 const isFile = require('../../util/isFile');
 const path = require('path');
 const addConfigsAsWebpackDependencies = require('../../util/addConfigsAsWebpackDependencies');
-const stringifyRequest = require('./utils/stringifyRequest')
+const stringifyRequest = require('./utils/stringifyRequest');
 
 /**
  * Allows you to import external files into a json value.
  * Can be used for any value, in an object or array.
  */
-module.exports = function RichmediaRCLoader(data) {
+
+module.exports = async function RichmediaRCLoader() {
   const callback = this.async();
   const options = this.getOptions();
   const loaderContext = this;
@@ -20,57 +20,49 @@ module.exports = function RichmediaRCLoader(data) {
 
   addConfigsAsWebpackDependencies(configFilepath, loaderContext); //recursively add richmediarc and sharedrc files as dependencies for webpack
 
-  let prom = Promise.resolve(config);
+  let data = typeof config === 'string' ? JSON.parse(config) : config;
+  data = JSON.parse(JSON.stringify(data));
 
-  prom = prom.then(() => {
-    return getRichmediaRC(configFilepath);
-  });
+  let ruuid = Date.now();
+  const replaceItems = [];
 
-  prom.then(data => {
-    data = typeof data === 'string' ? JSON.parse(data) : data;
-    data = JSON.parse(JSON.stringify(data));
+  if (data && data.content) {
+    leafs(data.content, (value, obj, name) => {
+      if (isFile(value) && !isExternalURL(value)) {
+        const id = `uuid_replace_${ruuid.toString(16)}`;
 
-    let ruuid = Date.now();
-    const replaceItems = [];
+        replaceItems.push({
+          key: stringifyRequest(loaderContext, id),
+          value: `require(${stringifyRequest(loaderContext, `${value}`)})`,
+        });
 
-    if (data && data.content) {
-      leafs(data.content, (value, obj, name) => {
-        if (isFile(value) && !isExternalURL(value)) {
-          const id = `uuid_replace_${ruuid.toString(16)}`;
+        this.addDependency(value);
 
-          replaceItems.push({
-            key: stringifyRequest(loaderContext, id),
-            value: `require(${stringifyRequest(loaderContext, `${value}`)})`,
-          });
+        obj[name] = id;
+      }
+    });
+  }
 
-          this.addDependency(value);
+  //convert the settings paths to a relative path
+  if (data && data.settings) {
+    leafs(data.settings, (value, obj, name) => {
+      if (isFile(value) && !isExternalURL(value)) {
+        obj[name] = "./" + path.basename(value)
+      }
+    });
+  }
 
-          obj[name] = id;
-        }
-      });
-    }
+  // remove contentSource / API key if it exists
+  if (data?.settings?.contentSource) delete data.settings.contentSource;
 
-    //convert the settings paths to a relative path
-    if (data && data.settings) {
-      leafs(data.settings, (value, obj, name) => {
-        if (isFile(value) && !isExternalURL(value)) {
-          obj[name] = "./" + path.basename(value)
-        }
-      });
-    }
+  data = JSON.stringify(data)
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029');
 
-    // remove contentSource / API key if it exists
-    if (data?.settings?.contentSource) delete data.settings.contentSource;
+  data = replaceItems.reduce((prev, item) => {
+    prev = prev.replace(item.key, item.value);
+    return prev;
+  }, data);
 
-    data = JSON.stringify(data)
-      .replace(/\u2028/g, '\\u2028')
-      .replace(/\u2029/g, '\\u2029');
-
-    data = replaceItems.reduce((prev, item) => {
-      prev = prev.replace(item.key, item.value);
-      return prev;
-    }, data);
-
-    callback(null, `module.exports = ${data};`);
-  });
+  callback(null, `module.exports = ${data};`);
 };
