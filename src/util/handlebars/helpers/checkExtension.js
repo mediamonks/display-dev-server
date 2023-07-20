e1caba7e1a644787c33b2293bcd1c4f09f9fb49e
Serhiy Fedorchuck <serhiy.fedorchuck@mediamonks.com>
2023-07-20 16:19:44 +0300

    remove comments

--- src/util/handlebars/helpers/checkExtension.js
+++ src/util/handlebars/helpers/checkExtension.js
@@ -1,6 +1,6 @@
 module.exports = function (value, checkStr, options) {
   let arr = checkStr.split('|');
-  
+
   for(let item of arr){
     if(value.endsWith(item)){
       return options.fn(this);
@@ -8,10 +8,4 @@ module.exports = function (value, checkStr, options) {
   }
 
   return options.inverse(this);
-
-  // if(value.endsWith('.png') || value.endsWith('.jpg') || value.endsWith('.jpeg')){
-  //   return options.fn(this);
-  // } else {
-  //   return options.inverse(this);
-  // }
 };
