f480ff7540e19a471c739542e80697a58cbcf7e9
Vasyl Ilba <vasyl.ilba@mediamonks.com>
2023-10-17 00:39:42 +0300

    new build parallel process with worker farm (faster for big amounts); new optional flag -p

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const getWebpackConfigs = require("./webpack/getWebpackConfigs");
 const devServer = require("./webpack/devServer");
 const buildFiles = require("./webpack/buildFiles");
+const buildFilesParallel = require("./webpack/buildFilesParallel");
 const buildPreview = require("./webpack/buildPreview");
 
 module.exports = async function (options) {
   // {mode = "development", glob = "./**/.richmediarc*", choices = null, stats = null, outputDir = "./build", configOverride = {}}
-  let {mode, glob, choices, stats, outputDir, skipBuild, skipPreview} = options;
+  let {mode, glob, choices, stats, outputDir, skipBuild, skipPreview, parallel} = options;
 
   const webpackConfigs = !skipBuild ? await getWebpackConfigs(options) : null;
 
   if (mode === "development") {
-    await devServer(webpackConfigs.result, webpackConfigs.choices.openLocation);
+      await devServer(webpackConfigs.result, webpackConfigs.choices.openLocation);
   } else {
-    if (!skipBuild) await buildFiles(webpackConfigs.result, outputDir);
+    if (!skipBuild) {
+      if (parallel) await buildFilesParallel(webpackConfigs.result, options);
+      else          await buildFiles(webpackConfigs.result, outputDir);
+    }
     if (!skipPreview) await buildPreview(outputDir);
   }
 };
---
+++ src/webpack/buildFilesParallel.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const removeTempRichmediaRc = require("../util/removeTempRichmediaRc");
+
+const cliProgress = require("cli-progress");
+
+const workerFarm = require('worker-farm');
+
+module.exports = async function buildFiles(result, options) {
+  const webpackRun = workerFarm(
+    {
+      autoStart: true,
+      maxRetries: 0,
+      maxConcurrentCallsPerWorker: 1,
+      maxConcurrentWorkers: options.parallel === true ? 4 : options.parallel
+    },
+    require.resolve('./webpackRun')
+  );
+  
+  const startTime = new Date().getTime();
+  const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  progressBar.start(result.length, 0);
+
+  await Promise.all(result.map((result) => {
+    delete result.settings.row
+    return new Promise(res => webpackRun({ config: result.settings, options }, () => {
+      progressBar.increment()
+      res()
+    }));
+  }));
+
+  workerFarm.end(webpackRun)
+
+  progressBar.stop();
+  console.log(`built in ${new Date().getTime() - startTime}ms`);
+
+  // final clean up
+  console.log("Removing temp .richmediarc...");
+  removeTempRichmediaRc(result);
+
+  return {
+    outputDir: path.resolve(options.outputDir),
+  };
+};
