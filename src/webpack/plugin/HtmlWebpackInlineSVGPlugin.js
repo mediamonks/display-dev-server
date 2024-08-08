fd2ec2e37aa8bbbc25bc465e903a76e186c3c602
Pablo Carreira <pablog.carreira@gmail.com>
2024-08-08 10:40:24 -0300

    mergePaths:false for the SVGO config

--- src/webpack/plugin/HtmlWebpackInlineSVGPlugin.js
+++ src/webpack/plugin/HtmlWebpackInlineSVGPlugin.js
@@ -461,6 +461,7 @@ class HtmlWebpackInlineSVGPlugin {
           params: {
             overrides: {
               cleanupIds: false,
+              mergePaths:false,
             },
           },
         },
